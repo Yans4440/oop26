b44bfd698c35cc2c41c3716592761e06f36b5bb5
Yans4440 <mryans4440@gmail.com>
2026-02-27 12:24:49 +0100

    lab1 zad 5

--- 1_svg/src/Main.java
+++ 1_svg/src/Main.java
@@ -1,3 +1,5 @@
+import com.sun.source.tree.NewArrayTree;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello word");
@@ -16,5 +18,43 @@ public class Main {
         s.p = p;
         s.q = p2;
         System.out.println(s.lengh());
+
+        Point p3 = new Point();
+        p3.x = 12;
+        p3.y = 7;
+        Point p4 = new Point();
+        p4.x = 8;
+        p4.y = 15;
+        Segment s2 = new Segment();
+        s2.q = p3;
+        s2.p = p4;
+
+        Point p5 = new Point();
+        p3.x = 42;
+        p3.y = 17;
+        Point p6 = new Point();
+        p4.x = 82;
+        p4.y = 25;
+        Segment s3 = new Segment();
+        s3.q = p5;
+        s3.p = p6;
+
+        Segment[] segments = new Segment[3];
+        segments[0] = s;
+        segments[1] = s2;
+        segments[2] = s3;
+
+        System.out.println(findMax(segments).p);
+
+    }
+    public static Segment findMax(Segment[] segments){
+        Segment maxSeg =segments[0];
+        for (Segment s: segments){
+            if(s.lengh()>maxSeg.lengh()) {
+                maxSeg = s;
+            }
+
+        }
+        return maxSeg;
     }
 }
