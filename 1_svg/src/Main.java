acb691a1d2e4b4bb19db652062709b68bad9092e
Yans4440 <mryans4440@gmail.com>
2026-02-27 11:34:00 +0100

    lab1 zad 1

--- 1_svg/src/Main.java
+++ 1_svg/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
-    public int x = 12;
     public static void main(String[] args){
         System.out.println("Hello word");
+        Point p = new Point();
+        p.x = 5F;
+        p.y = 7.5F;
+
+        System.out.println(p.toString());
     }
 }
---
+++ 1_svg/src/Point.java
@@ -0,0 +1,8 @@
+public class Point {
+    public float x;
+    public float y;
+
+    public String toString(){
+        return "Point {x="+x+",y="+y+"}";
+    };
+}
