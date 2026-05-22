0f7767f69a38525a74b0412f6f51402128dad268
Yans4440 <mryans4440@gmail.com>
2026-05-22 11:30:15 +0200

    lab9 sqlite-2

--- site_v2/src/main/java/Main.java
+++ site_v2/src/main/java/Main.java
@@ -1,15 +1,14 @@
 import database.DatabaseConnection;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import javax.annotation.processing.SupportedSourceVersion;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args){
         DatabaseConnection db = DatabaseConnection.getInstance();
         try {
             db.connect("/home/student/Dokumenty/oop26/oop26/site_v2/users.db");
+            insert("Jan", "12312");
             select();
             db.disconnect();
         }catch (SQLException e){
@@ -31,6 +30,23 @@ public class Main {
             String name = rs.getString("username");
             String pass = rs.getString("password");
             System.out.println("ID: " + id + " NAME: " + name + " PASSWORD: " + pass);
+        }
+    }
+    public static void insert(String name, String password) throws SQLException {
+        Connection connection = DatabaseConnection.getInstance().getConnection();
+        String query = "INSERT INTO account (username, password) VALUES(?,?)";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, name);
+        preparedStatement.setString(2, password);
+        preparedStatement.execute();
+
+        ResultSet resultSet= preparedStatement.getGeneratedKeys();
+
+        while (resultSet.next()){
+            int id = resultSet.getInt(1);
+            System.out.println("Created ID: " + id);
+
         }
     }
 
