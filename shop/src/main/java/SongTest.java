6bc034fb9480480603f788a63d62ef9b70eca539
Yans4440 <mryans4440@gmail.com>
2026-05-29 12:25:58 +0200

    lab9 errorDatabase 2

--- shop/src/main/java/SongTest.java
+++ shop/src/main/java/SongTest.java
@@ -1,8 +1,13 @@
+import com.sun.source.tree.AssertTree;
 import music.Song;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.internal.invokers.Arguments;
 
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertTrue;
@@ -18,5 +23,32 @@ public class SongTest {
         Song expected = new Song("Queen","Bohemian Rhapsody",355);
         assertTrue(song.isPresent());
         assertEquals(expected, song.get());
+    }
+
+    @Test
+    public void testReadFail() throws SQLException{
+        DatabaseConnection.connect("songs.db");
+        Optional<Song> song = Song.Persistence.read(68);
+        assertTrue(song.isEmpty());
+    }
+
+    private static Stream<Arguments> args(){
+        return Stream.of(
+                argumemts(40, "The Beatles","Help!",138),
+                argumemts(37,"The Doors","Hello, I Love You",136)
+        );
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("args")
+    public void testReadMany(int id, String artist, String title, int length){
+        DababaseConnection.connect("songs.db", "");
+
+        Optional<Song> song = Song.Persistence.read(id);
+        Song expectedSong = new Song(artist,title,length);
+
+        assertTrue(song.isPresent());
+        assertEquals(expectedSong, song.get());
     }
 }
