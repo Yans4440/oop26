08a5e108dd4f59eebafa5832fc3b72f066412a3b
Yans4440 <mryans4440@gmail.com>
2026-05-29 11:30:15 +0200

    lab9 czas

--- shop/src/main/java/PlaylistTest.java
+++ shop/src/main/java/PlaylistTest.java
@@ -1,9 +1,10 @@
 import music.Playlist;
 import music.Song;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.IndicativeSentencesGeneration;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class PlaylistTest {
@@ -53,6 +54,29 @@ public class PlaylistTest {
         assertEquals(song1, playlist.atSecond(50));
         assertEquals(song2, playlist.atSecond(200));
         assertEquals(song3, playlist.atSecond(300));
+    }
+
+    private IndexOutOfBoundsException doesThrowExceptionCommon(int seconds){
+        Playlist playlist = new Playlist();
+        Song song1 = new Song("Deftones", "Genisis", 150);
+        Song song2 = new Song("Muse", "test2", 112);
+        Song song3 = new Song("Lavina", "Kraj mene", 181);
+
+        playlist.add(song1);
+        playlist.add(song2);
+        playlist.add(song3);
+
+        return assertThrows(IndexOutOfBoundsException.class, () -> playlist.atSecond(seconds));
+    }
+
+    @Test
+    public void testDoesThrowException(){
+        assertEquals("Zbyt duży czas", doesThrowExceptionCommon(1500).getMessage());
+    }
+
+    @Test
+    public void testDoesThrowNegativeException(){
+        assertEquals("Ujemny czas", doesThrowExceptionCommon(-1000).getMessage());
     }
 }
 
